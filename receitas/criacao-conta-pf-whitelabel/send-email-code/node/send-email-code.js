5e2a5933d025d487563514fbbd63d559de713e76
Gabriel Rezende <rezende.f.gabriel@gmail.com>
2023-07-23 10:05:17 -0300

    changes the request to pass the parameters in the body

--- receitas/criacao-conta-pf-whitelabel/send-email-code/node/send-email-code.js
+++ receitas/criacao-conta-pf-whitelabel/send-email-code/node/send-email-code.js
@@ -1,10 +1,11 @@
 const axios = require('axios');
 
 async function sendEmailCode() {
-    const baseUrl = 'https://whitelabel-sandbox.delbank.dev.br/v1/customers/send-email-code?email=sotex95831@rc3s.com';
+    const baseUrl = 'https://whitelabel-sandbox.delbank.dev.br/v1/customers/send-email-code';
+    const emailAddress = 'josevaldo@peixoto.com.br';
 
     try {
-        const response = await axios.post(baseUrl);
+        const response = await axios.post(baseUrl, { email: emailAddress });
 
         console.log(response.data);
     } catch (error) {
