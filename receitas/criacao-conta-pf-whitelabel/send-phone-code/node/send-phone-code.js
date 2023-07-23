824d8faaaa60cdc62287905d77aeb07b877e4f54
Gabriel Rezende <rezende.f.gabriel@gmail.com>
2023-07-23 10:02:35 -0300

    changes the request to pass the parameters in the body

--- receitas/criacao-conta-pf-whitelabel/send-phone-code/node/send-phone-code.js
+++ receitas/criacao-conta-pf-whitelabel/send-phone-code/node/send-phone-code.js
@@ -1,10 +1,11 @@
 const axios = require('axios');
 
 async function sendPhoneCode() {
-    const baseUrl = 'https://whitelabel-sandbox.delbank.dev.br/v1/customers/send-phone-code?phone=79900000000';
+    const baseUrl = 'https://whitelabel-sandbox.delbank.dev.br/v1/customers/send-phone-code';
+    const phoneNumber = '79900000000';
 
     try {
-        const response = await axios.post(baseUrl);
+        const response = await axios.post(baseUrl, { phone: phoneNumber });
 
         console.log(response.data);
     } catch (error) {
